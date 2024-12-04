096ad9dadde3a91add2282ab4b7b0e1a4a460079
LordMakaron <salnikov_18@inbox.ru>
2024-12-04 19:20:13 +0300

    Добавил ввод двух целых чисел

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 // main.cpp
 #include "sdt.h"
+#include <iostream>
 
 int main() {
+    int a, b;
+    std::cout << "Enter chislo A: ";
+    std::cin >> a;
+    std::cout << "Enter chislo B: ";
+    std::cin >> b;
     return 0;
 }
+
