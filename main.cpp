3b9665088a2abb4e141e3af2c1cb67f7104b66a2
LordMakaron <salnikov_18@inbox.ru>
2024-12-05 19:10:27 +0300

    Добавил комментарий, что программа является учебной

--- main.cpp
+++ main.cpp
@@ -1,17 +1,15 @@
 // main.cpp
 #include "sdt.h"
 #include <iostream>
-
+//Программа является учебной
 int main() {
     int a, b;
     std::cout << "Enter chislo A: ";
     std::cin >> a;
     std::cout << "Enter chislo B: ";
     std::cin >> b;
-
     int sum = a + b; 
     std::cout << "Summa: " << sum << std::endl;
-
     return 0;
 }
 
